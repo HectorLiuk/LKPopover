5bed4855a477ed9cc918b51c8fdcc580d51a2169
snail_lk <544523660@qq.com>
2016-03-29 17:59:54 +0800

    layoutIfNeeded

--- LKPopover/ViewController.m
+++ LKPopover/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *numLabel;
+@property (weak, nonatomic) IBOutlet UIButton *clickBtn;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *top;
 
 @end
 
@@ -17,6 +20,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)click:(id)sender {
+
+    self.top.constant = 100;
+    [self.view layoutIfNeeded];
+
+    [UIView animateWithDuration:1.0f delay:0.3f usingSpringWithDamping:0.1 initialSpringVelocity:1 options:UIViewAnimationOptionAllowAnimatedContent animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        
+    }];
+    
+    
+    
+
+//    [UIView animateWithDuration:0.5 delay:0 options:UIViewAnimationOptionBeginFromCurrentState | UIViewAnimationOptionCurveEaseInOut animations:^{
+//        self.numLabel.transform = CGAffineTransformMakeScale(1.5, 1.5)  ;
+//    } completion:^(BOOL finished) {
+//        
+//    }];
+
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
