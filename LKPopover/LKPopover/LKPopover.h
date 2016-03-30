1e8c9282add6cedfa7a4fa06e33d0207d2cbace8
snail_lk <544523660@qq.com>
2016-03-30 18:36:38 +0800

    property

--- LKPopover/LKPopover/LKPopover.h
+++ LKPopover/LKPopover/LKPopover.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, LKPopoverPositionType) {
+    LKPopoverPositionTypeUp = 1,//箭头在content上面
+    LKPopoverPositionTypeDown,
+    LKPopoverPositionTypeLeft,
+    LKPopoverPositionTypeRight,
+};
+typedef NS_ENUM(NSUInteger, LKPopoverMaskType) {
+    LKPopoverMaskTypeGray = 1,//有蒙版效果,支持touch视图消失
+    LKPopoverMaskTypeNone,//没有蒙版效果
+};
+typedef void(^animationBlock)(void);
+
 @interface LKPopover : UIView
 
 + (instancetype)popover;
@@ -17,20 +29,40 @@
  */
 @property (nonatomic, assign) UIEdgeInsets contentInset;
 /**
- *  提示窗箭头尺寸,
+ *  提示窗箭头尺寸 {10，10},
  */
 @property (nonatomic, assign) CGSize arrowSize;
 /**
- *  边角弧度
+ *  边角弧度 默认5.0f
  */
 @property (nonatomic, assign) CGFloat cornerRadius;
 /**
- *  点击提示窗动画持续时间
+ *  点击提示窗动画持续时间 默认0.5f
  */
 @property (nonatomic, assign) CGFloat animationShow;
 /**
- *  取消提示窗动画持续时间
+ *  取消提示窗动画持续时间 默认0.5f
  */
 @property (nonatomic, assign) CGFloat animationDismss;
+/**
+ *  开启spring动画效果 默认是开启的
+ */
+@property (nonatomic, assign, getter = isAnimationSpring) BOOL animationSpring;
+/**
+ *  回调 处理提示窗弹出后事务处理
+ */
+@property (nonatomic, copy) animationBlock didShowHandler;
+/**
+ *  回调 处理提示窗消失后事务处理
+ */
+@property (nonatomic, copy) animationBlock didDismssHandler;
+/**
+ *  底部蒙版类型,默认是LKPopoverMaskTypeGray
+ */
+@property (nonatomic, assign) LKPopoverMaskType maskType;
+/**
+ *  箭头在需要提示窗的位置
+ */
+@property (nonatomic, assign) LKPopoverPositionType positionType;
 
 @end
